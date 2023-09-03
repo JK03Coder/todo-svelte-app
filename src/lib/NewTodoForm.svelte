26392e095bf206c748a4c1be4ff755c543465c76
JK03Coder <jnkempton@gmail.com>
2023-09-04 02:02:56 +1200

    add todo functionality

--- src/lib/NewTodoForm.svelte
+++ src/lib/NewTodoForm.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
+  import { todos } from "./stores";
+
+  let todoValue: string;
+
+  function handleNewTodo() {
+    if (todoValue.trim() === "") return;
+    todos.addTodo(todoValue);
+    todoValue = "";
+  }
 </script>
 
-<form class="flex flex-col gap-2 mb-6">
+<form class="flex flex-col gap-2 mb-6" on:submit|preventDefault={handleNewTodo}>
   <div>
     <label class="my-1 block py-0.5 dark:text-white" for="item">New Todo</label>
     <input
       class="w-full rounded-md border-2 border-blue-400 bg-blue-400/50 py-0.5 pl-1.5 text-black shadow shadow-blue-400/40 outline-none transition-all placeholder:text-blue-400 focus:shadow-blue-400/90 dark:text-white"
       type="text"
       id="item"
-      placeholder="add an item"
+      placeholder="add a todo"
+      bind:value={todoValue}
     />
   </div>
   <button
